bb91f44faa190e10d3896c8bb4086ba3ee470a7a
gaosongm <gaosongm@yonyou.com>
2018-07-31 13:37:18 +0800

    测试

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,8 +1,34 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test {
+	
 	public static void main(String[] args) {
-		System.out.println("ABC");
-		System.out.println("增加一行");
+		String str = "pk_user in (select cuserid from sm_user where pk_org in ('0001A110000000000A2Y','0001A110000000002DQ9')) AND tempstyle in ('鍗曟嵁妯℃澘','鏌ヨ妯℃澘') AND pk_user in ('0001A11000000000288Y','0001A110000000002DNJ')";
+	    String[] values = str.split("AND");	
+	    String[] result = null;
+	    
+	    Map<String,String[]> map = new HashMap<String,String[]>();
+	    
+	    
+	    Pattern p = Pattern.compile("\'.*\'");
+	    Matcher m = p.matcher(values[0]);
+	    List<String> list = new ArrayList<String>();
+	    int index = 0;
+	    String pk = null;
+	    while(m.find()){
+	    	pk = m.group(0);
+	    	list.add(pk.replace("\'", ""));
+//	    	index++;
+	    }
+	    System.out.println(list);
+		
 	}
+
 }
